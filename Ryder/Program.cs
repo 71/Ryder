153624b2b91fb1acd2cbde68e662d2c1f5c00b98
Greg G <m-ae.j@outlook.com>
2017-07-01 10:44:01 +0200

    Add project files.

---
+++ Ryder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ryder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
